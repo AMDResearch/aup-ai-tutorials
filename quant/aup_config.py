b68095c1ef9ab38c6e6d2b077722635277d40dc4
Mario Ruiz <mruiznog@amd.com>
2026-03-10 17:02:07 +0000

    Update quark

--- quant/aup_config.py
+++ quant/aup_config.py
@@ -57,10 +57,12 @@ def aup_setup() -> None:
     logging.info("Pip upgraded installed %s.", message_string(proc))
 
     proc = run_capture(["pip", "install", "matplotlib", "ml_dtypes", "tabulate",
-                        "onnxruntime", "onnx>=1.16.2",
-                        "onnxscript", "pygit2", "lm_eval==0.4.9.2",
-                        "optimum[amd]", "onnxruntime_genai",
-                        "amd-quark==0.10"],
+                        "onnxruntime", "onnx>=1.16.2", "onnxscript",
+                        "pygit2", "lm_eval==0.4.9.2", "optimum[amd]",
+                        "onnxruntime_genai"], check=True)
+
+    proc = run_capture(["pip", "install",
+                        "git+https://github.com/amd/Quark/@release/0.11"],
                        check=True)
 
     if amd_dev_cloud:
